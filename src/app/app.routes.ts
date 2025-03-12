e22e77c6de7c4e233d8e8275938c8d45591e8806
Bakops <bakoutoure4@gmail.com>
2025-03-12 13:38:28 +0100

    Problème #1: Navigation incorrecte VALIDER

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,21 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookDetailComponent } from './components/book-detail/book-detail.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
+import { NgModule } from '@angular/core';
 
 // TODO 5 : Créer une route pour la page détail d'un livre
 export const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', component: HomeComponent },
   { path: 'books', component: BookListComponent },
   { path: 'books/add', component: AddBookComponent },
   { path: 'books/:id', component: BookDetailComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
 ];
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+})
+export class AppRoutingModule {}
